b501b02bc6f4113dba73b68fb039729cfe85dd7c
sinoquei <j_siqueiros@outlook.com>
2022-09-06 10:56:50 -0500

    Product and color objects added

---
+++ source/color.js
@@ -0,0 +1,16 @@
+const Colors = {
+    main: '#48b600',
+    white: '#fff',
+    black: '#000',
+    lightBlack: '#bcbcbc',
+    orange: '#ffce31',
+    red: '#ff0000',
+    gray: '#f4f4f4',
+    subGreen: '#eefae6',
+    deepGray: '#c4c4c4',
+    blue: '#3b87f9',
+    underline: '#e5e5e5',
+    paypal: '#ffb730'
+};
+
+export default Colors;
---
+++ source/data/Products.js
@@ -0,0 +1,144 @@
+const products = [
+    {
+        id: '0',
+        name: 'Magenta Oversized Turtleneck',
+        image: '../../assets/images/1.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 55,
+        countInStock: 3,
+        rating: 4,
+        numReviews: 4
+    },
+    {
+        id: '1',
+        name: 'Daisy Print Tee',
+        image: '../../assets/images/2.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 35,
+        countInStock: 4,
+        rating: 3,
+        numReviews: 4
+    },
+    {
+        id: '2',
+        name: 'Black Pleated Cover',
+        image: '../../assets/images/3.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 155,
+        countInStock: 2,
+        rating: 5,
+        numReviews: 3
+    },
+    {
+        id: '3',
+        name: 'Leather Vest',
+        image: '../../assets/images/4.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 98,
+        countInStock: 6,
+        rating: 4,
+        numReviews: 5
+    },
+    {
+        id: '4',
+        name: 'Janet Jackson Tour Tee',
+        image: '../../assets/images/5.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 105,
+        countInStock: 1,
+        rating: 5,
+        numReviews: 1
+    },
+    {
+        id: '5',
+        name: 'Brown Bomber Jacket',
+        image: '../../assets/images/6.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 78,
+        countInStock: 2,
+        rating: 3,
+        numReviews: 4
+    },
+    {
+        id: '6',
+        name: 'Raf Black Dress',
+        image: '../../assets/images/7.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 67,
+        countInStock: 3,
+        rating: 4,
+        numReviews: 4
+    },
+    {
+        id: '7',
+        name: 'Stained Glass Button-up',
+        image: '../../assets/images/8.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 45,
+        countInStock: 5,
+        rating: 5,
+        numReviews: 4
+    },
+    {
+        id: '8',
+        name: 'Sheer Camel Jacket',
+        image: '../../assets/images/9.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 83,
+        countInStock: 2,
+        rating: 5,
+        numReviews: 2
+    },
+    {
+        id: '9',
+        name: 'Yellow Athletic Trainers',
+        image: '../../assets/images/10.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 122,
+        countInStock: 2,
+        rating: 3,
+        numReviews: 2
+    },
+    {
+        id: '10',
+        name: 'Leather Handbag',
+        image: '../../assets/images/11.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 330,
+        countInStock: 1,
+        rating: 5,
+        numReviews: 3
+    },
+    {
+        id: '11',
+        name: 'Champagne Dress',
+        image: '../../assets/images/12.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 268,
+        countInStock: 2,
+        rating: 5,
+        numReviews: 2
+    },
+    {
+        id: '12',
+        name: 'Issey Miyake 2 Piece Set',
+        image: '../../assets/images/13.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 223,
+        countInStock: 1,
+        rating: 5,
+        numReviews: 2
+    },
+    {
+        id: '13',
+        name: 'Black Dress Jacket',
+        image: '../../assets/images/14.png',
+        description: 'Cupidatat deserunt aliquip consectetur voluptate commodo qui sit nostrud in incididunt veniam.',
+        price: 111,
+        countInStock: 4,
+        rating: 4,
+        numReviews: 4
+    },
+];
+
+export default products;
