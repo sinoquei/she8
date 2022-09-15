d3920daf1c0e0a426d516c54fa3e6d5a0041cbf3
sinoquei <j_siqueiros@outlook.com>
2022-09-15 18:06:24 -0500

    Bottom Navigation

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
-import { NativeBaseProvider } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import LoginScreen from './source/screens/LoginScreen';
+import RegisterScreen from './source/screens/RegisterScreen';
 import OrderScreen from './source/screens/OrderScreen';
+import BottomNav from './source/navigation/BottomNav';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <OrderScreen />
+      <NavigationContainer>
+        <StatusBar hidden={true} />
+        <Stack.Navigator 
+          initialRouteName='Bottom' 
+          screenOptions={{
+          headerShown:false
+        }}
+        >
+          <Stack.Screen 
+            name='Login'
+            component={LoginScreen}
+          />
+          <Stack.Screen 
+            name='Register'
+            component={RegisterScreen}
+          />
+          <Stack.Screen 
+            name='Order'
+            component={OrderScreen}
+          />
+          <Stack.Screen 
+            name='Bottom'
+            component={BottomNav}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   )
   
